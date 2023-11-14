38a3f60df8f215a55666f49bcb627e50ffdc1371
William Rodrigues <william.dg88@gmail.com>
2023-11-14 08:37:48 -0300

    Ajustando requisições POST/PUT/DELETE

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.mapper.TopicoFormMapper
@@ -17,8 +18,8 @@ class TopicoService(
 ) {
 
     fun listar(): List<TopicoView> {
-        return topicos.stream().map {
-                t -> topicoViewMapper.map(t)
+        return topicos.stream().map { t ->
+            topicoViewMapper.map(t)
         }.collect(Collectors.toList())
     }
 
@@ -29,9 +30,35 @@ class TopicoService(
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(form: NovoTopicoForm) {
+    fun cadastrar(form: NovoTopicoForm): TopicoView {
         val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+        return topicoViewMapper.map(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
+        val topico = topicos.stream().filter { t ->
+            t.id == form.id
+        }.findFirst().get()
+        val topicoAtualizado = Topico(
+            id = form.id,
+            titulo = form.titulo,
+            mensagem = form.mensagem,
+            autor = topico.autor,
+            curso = topico.curso,
+            respostas = topico.respostas,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        )
+        topicos = topicos.minus(topico).plus(topicoAtualizado)
+        return topicoViewMapper.map(topicoAtualizado)
+    }
+
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+        topicos = topicos.minus(topico)
     }
 }
